021ee8b5caa8de6c983a0a60c551f794cf765e7e
p5eudoDjnn <alexander.havers@gmail.com>
2022-09-19 15:33:28 -0400

    heroku test

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const app = express();
-
 // creating a route after imports
 const { animals } = require("./data/animals");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -48,25 +49,6 @@ function filterByQuery(query, animalsArray) {
   // return the filtered results:
   return filteredResults;
 }
-function filterByQuery(query, animalsArray) {
-  let filteredResults = animalsArray;
-  if (query.diet) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.diet === query.diet
-    );
-  }
-  if (query.species) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.species === query.species
-    );
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(
-      (animal) => animal.name === query.name
-    );
-  }
-  return filteredResults;
-}
 
 app.get("/api/animals", (req, res) => {
   let results = animals;
@@ -76,6 +58,6 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log("API server now on port 3001");
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 });
